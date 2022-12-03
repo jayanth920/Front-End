324106ea58201e41b9be24c265708ac93900437e
humza15 <humzazafarullah@gmail.com>
2022-12-03 13:59:56 -0500

    sumbit

---
+++ to-do-list/src/components/Page.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+function Page () {
+    
+   
+
+    return (
+        <div>
+            <h1> To Do List  </h1>
+          <div className="Link">
+               <form>
+               <input className="button" type ="submit" value="Read more" />
+               </form> 
+            </div> 
+        </div>
+    )
+}
+
+
+export default Page
