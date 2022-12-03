d2c4aba10b84b26649a897f677d4447202634fdb
humza15 <humzazafarullah@gmail.com>
2022-12-03 16:34:01 -0500

    data sample

--- to-do-list/src/components/Page.js
+++ to-do-list/src/components/Page.js
@@ -5,12 +5,16 @@ import React from "react";
 function Page () {
     
    
-
+    const searchFood=(evt)=> {
+        if (evt.key === "Enter"){
+            
+        }
+    }
     return (
         <div>
             <h1> To Do List  </h1>
         <div className="search">
-                <input type="search" className="search" placeholder="Search todo list" /> 
+                <input type="search" className="search" placeholder="Search todo list" onChange={e=>setSearch(e.target.value)} onKeyPress={searchFood} /> 
          </div> 
         </div>
     )
