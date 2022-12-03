880089284b38e0b870f3c69af0404f65daaaa60f
humza15 <humzazafarullah@gmail.com>
2022-12-03 16:58:14 -0500

    data added

---
+++ to-do-list/src/components/Data.js
@@ -0,0 +1,25 @@
+const thingsToDo =[
+    {  
+        "task": "Package delivery at 11am"
+    },
+    {
+        "task": "Have lunch at 12 pm"
+    },
+    {
+        "task": "Meet up with friends at 2pm"
+    },
+    {
+        "task": "Go to the doctors at 3pm"
+    },
+    {
+        "task": "Go home at 4pm and clean house"
+    },
+    {
+        "task": "Do homework at 5pm"
+    },
+    {
+        "task":"Go to sleep at 8pm"
+    }
+    ]
+    
+    export default thingsToDo
