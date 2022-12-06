d2d9e640d9927b2eb8b18e415bddec433d33fdf6
jayanth920 <113549918+jayanth920@users.noreply.github.com>
2022-12-06 17:12:39 -0600

    corrected backend link by removing slash

--- src/utils/HandleApi.js
+++ src/utils/HandleApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = "https://autumn-butterfly-3437.fly.dev/"
+const baseUrl = "https://autumn-butterfly-3437.fly.dev"
 
 const getAllToDo = (setToDo) => {
     axios
